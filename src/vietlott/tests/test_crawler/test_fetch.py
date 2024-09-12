04d415cf956e73af7f0075efc821ee767d343b20
vietvudanh <vietvudanh@gmail.com>
2024-09-12 08:16:04 +0700

    add test for 3dpro

--- src/vietlott/tests/test_crawler/test_fetch.py
+++ src/vietlott/tests/test_crawler/test_fetch.py
@@ -1,6 +1,7 @@
 import cattrs
 
-from vietlott.crawler.products.p3d import RequestP3D
+from vietlott.crawler.products import P3DPro
+from vietlott.crawler.products.p3d import P3D
 from vietlott.crawler.products.power645 import ProductPower645
 from vietlott.crawler.products.power655 import ProductPower655
 from vietlott.crawler.requests_helper import config as requests_config
@@ -52,10 +53,30 @@ def test_power_3d():
     # vietlott_cookie, cookies = get_vietlott_cookie()
     vietlott_cookie, cookies = None, None
     fn_fetch = fetch_wrapper(
-        RequestP3D.url,
+        P3D.url,
         dict(Cookie=vietlott_cookie, **requests_config.headers),
-        RequestP3D.org_params,
-        cattrs.unstructure(RequestP3D.org_body),
+        P3D.org_params,
+        cattrs.unstructure(P3D.org_body),
+        _fn,
+        cookies,
+    )
+    resp = fn_fetch([{"task_id": "1", "task_data": {"params": {}, "body": {"PageIndex": 1}}}])
+    print(resp)
+
+    assert len(resp) == 1, "some task fail"
+
+
+def test_power_3d_pro():
+    def _fn(params, body, parsed_json, task_data):
+        return parsed_json
+
+    # vietlott_cookie, cookies = get_vietlott_cookie()
+    vietlott_cookie, cookies = None, None
+    fn_fetch = fetch_wrapper(
+        P3DPro.url,
+        dict(Cookie=vietlott_cookie, **requests_config.headers),
+        P3DPro.org_params,
+        cattrs.unstructure(P3DPro.org_body),
         _fn,
         cookies,
     )
