01fdae3b1e46b6a98e562c2480c2a2ca88f7daa6
Viet VU <vietvudanh@gmail.com>
2024-12-23 16:43:18 +0700

    update headers

--- src/vietlott/crawler/requests_helper/config.py
+++ src/vietlott/crawler/requests_helper/config.py
@@ -14,22 +14,20 @@ cwd = Path(__file__).parent
 # }
 
 headers = {
-    "Host": "vietlott.vn",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:128.0) Gecko/20100101 Firefox/128.0",
     "Accept": "*/*",
-    # "Accept-Language": "en-US,en;q=0.9,vi;q=0.8",
-    "Accept-Language": "vi;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Content-Type": "text/plain; charset=utf-8",
-    "X-AjaxPro-Method": "ServerSideDrawResult",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Content-Type": "application/octet-stream",
+    "X-Requested-With": "XMLHttpRequest",
+    "X-csrftoken": "1813c50925a5f035fe1d472bf49fd2760de7f4694b082b2d5ba8a2aad77c7886dc16b6ac9946c6fb",
+    "X-Ajax-Token": "f5cfca365b94035ece913dd1d70f7d9e91597ca23872847b74bc2f5d84cb1356",
     "Origin": "https://vietlott.vn",
     "Connection": "keep-alive",
     "Referer": "https://vietlott.vn/vi/trung-thuong/ket-qua-trung-thuong/winning-number-655",
-    # "Cookie": "D1N=4ba207a670090cbab6b026f2d64e4d5b",
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
-    "TE": "trailers",
+    "Priority": "u=0",
 }
 
 TIMEOUT = 20
