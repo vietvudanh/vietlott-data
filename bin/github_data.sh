9ccd2df8eb24dbf95d6707c6e122f7e180896e0e
Viet VU <vietvudanh@gmail.com>
2025-08-01 11:42:07 +0700

    add 535 to run

--- bin/github_data.sh
+++ bin/github_data.sh
@@ -25,6 +25,8 @@ python src/vietlott/cli/crawl.py power_655
 python src/vietlott/cli/missing.py power_655
 python src/vietlott/cli/crawl.py power_645
 python src/vietlott/cli/missing.py power_645
+python src/vietlott/cli/crawl.py power_535
+python src/vietlott/cli/missing.py power_535
 python src/vietlott/cli/crawl.py 3d
 python src/vietlott/cli/missing.py 3d
 python src/vietlott/cli/crawl.py 3d_pro
